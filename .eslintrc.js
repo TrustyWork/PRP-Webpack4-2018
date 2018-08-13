750997874019a6f0cc8b57aa3fabc6a76208731c
TrustyWork <trustywork@gmail.com>
2018-08-13 04:58:56 +0300

    fix js style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,46 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "airbnb"
+  extends: [
+    'eslint:recommended',
+    'airbnb',
   ],
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 6,
-    "sourceType": "module"
+    ecmaVersion: 6,
+    sourceType: 'module',
   },
-  "plugins": [
-    "react"
+  plugins: [
+    'react',
   ],
-  "rules": {
-    "indent": [
-      "error",
-      2
+  rules: {
+    indent: [
+      'error',
+      2,
     ],
-    "linebreak-style": [
-      "error",
-      "unix"
+    'linebreak-style': [
+      'error',
+      'unix',
     ],
-    "quotes": [
-      "error",
-      "single"
+    quotes: [
+      'error',
+      'single',
     ],
-    "no-console": [
-      "off"
+    'no-console': [
+      'off',
     ],
-    "max-len": [
-      "error",
+    'max-len': [
+      'error',
       {
-        "code": 150,
-        "ignoreTrailingComments": true
-      }
-    ]
+        code: 150,
+        ignoreTrailingComments: true,
+      },
+    ],
   },
 };
